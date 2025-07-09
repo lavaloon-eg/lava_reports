594381ad9deda4039fbc718f27d551456f36e54f
AMRRAGHEB118 <amrragheb118@gmail.com>
2025-07-09 11:37:03 +0300

    Improve data handling and add user details

--- lava_reports/lava_reports/report/unposted_transactions/unposted_transactions.js
+++ lava_reports/lava_reports/report/unposted_transactions/unposted_transactions.js
@@ -12,13 +12,17 @@ frappe.query_reports["Unposted Transactions"] = {
 		},
 		{
 			"fieldname": "doctype",
-			"label": __("Document Type"),
+			"label": __("Transaction Type"),
 			"fieldtype": "Select",
 			"options": [
 				"",
-				"Sales Invoice",
 				"Quotation",
 				"Sales Order",
+				"Delivery Note",
+				"Sales Invoice",
+				"Purchase Order",
+				"Purchase Receipt",
+				"Purchase Invoice",
 				"Payment Entry"
 			],
 			"reqd": 0
@@ -28,13 +32,22 @@ frappe.query_reports["Unposted Transactions"] = {
 		value = default_formatter(value, row, column, data);
 
 		if (column.fieldname === "id" && data.type) {
-			const doc_type_slug = data.type.toLowerCase().replace(/\s+/g, '-');
+			const doc_type_slug = frappe.router.slug(data.type)
 
 			value = `<a href="/app/${doc_type_slug}/${data.id}" 
 						data-doctype="${data.type}" 
 						data-name="${data.id}" 
 						data-value="${data.id}">
 						${data.id}
+					</a>`;
+		}
+
+		if (column.fieldname === "full_name" && data.full_name && data.created_by) {
+			value = `<a href="/app/user/${data.created_by}" 
+						data-doctype="User" 
+						data-name="${data.created_by}" 
+						data-value="${data.created_by}">
+						${data.full_name}
 					</a>`;
 		}
 
